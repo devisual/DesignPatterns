c20efc2d707ed570ddf3be30651a1fd45ecc0f0b
Sandra Marcinek <s.marcinek17@outlook.com>
2018-11-20 19:22:08 +0100

    - add Singleton example

---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(User.Instance.GetUserCounter());
+            Console.WriteLine(User.Instance.GetUserCounter());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    sealed class User
+    {
+        static User _user;
+        int UserCount;
+        public static User Instance
+        {
+            get
+            {
+                if (_user == null)
+                {
+                    _user = new User();
+                }
+                return _user;
+            }     
+        }
+        private User()
+        {
+            UserCount = 1;
+        }
+        public int GetUserCounter()
+        {
+            UserCount++;
+            return UserCount;
+        }
+    }
+}
